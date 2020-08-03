2883e88037f2b56df344392b7a20d52781cd68af
WS <wskeow13@gmail.com>
2020-08-03 23:46:57 +0800

    Minor clean-up in app.js.

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,52 +5,52 @@ angular.module('NarrowItDownApp',[])
 .controller('NarrowItDownController',NarrowItDownController)
 .service('MenuSearchService',MenuSearchService)
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-.directive('foundItems',FoundItems);
+.directive('foundItems',FoundItemsDirective);
 
-function FoundItems() {
+function FoundItemsDirective(){
   var ddo = {
-    templateUrl : 'foundItems.html',
-    scope : {
-      onRemove : '&',
-      items : '<'
+    templateUrl: 'foundItems.html',
+    scope: {
+      onRemove: '&',
+      items: '<'
     }
   };
   return ddo;
 }
 
 NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
+function NarrowItDownController(MenuSearchService){
     var menu = this;
-    menu.menuSearch = function (searchTerm) {
+    menu.menuSearch = function (searchTerm){
       var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-      promise.then(function (response) {
+      promise.then(function (response){
         menu.found = response;
       })
-    .catch( function (error) {
+    .catch( function (error){
       console.log('Error while retrieving data.');
     });
   };
-  menu.removeItem = function (index) {
+  menu.removeItem = function (index){
     menu.found.splice(index,1);
   }
 }
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-function MenuSearchService($http, ApiBasePath) {
+function MenuSearchService($http, ApiBasePath){
   var service = this;
-  service.getMatchedMenuItems = function (searchTerm) {
+  service.getMatchedMenuItems = function (searchTerm){
     return $http({
       method : "GET",
       url : (ApiBasePath + "/menu_items.json")
     })
-    .then(function (result) {
-      var dat = result.data;
+    .then(function (result){
+      var mdata = result.data;
       var fditems=[];
-        for (var i in dat.menu_items) {
-          if (dat.menu_items[i].description.indexOf(searchTerm) === -1) {
+        for (var i in mdata.menu_items){
+          if (mdata.menu_items[i].description.indexOf(searchTerm) === -1){
             console.log("Not found.");
           }
-          else fditems.push(dat.menu_items[i]);
+          else fditems.push(mdata.menu_items[i]);
           }
          console.log(fditems);
          return fditems;
